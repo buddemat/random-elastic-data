3e24e36f6bccb940f94d5121a69ff16f0fe22f81
Matthias Budde <buddemat@users.noreply.github.com>
2023-03-06 09:47:53 +0100

    Feat: Added numid class var.

--- random_person.py
+++ random_person.py
@@ -3,8 +3,12 @@ from datetime import datetime
 
 class RandomPerson:
     """Class for random person"""
-    def __init__(self):
+    _instance_count = 0
+
+    def __init__(self, uuid=str(uuid.uuid1())):
+        RandomPerson._instance_count += 1
         self.created_at = datetime.today() # created_at.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
-        self.uuid = str(uuid.uuid1())
+        self.uuid = uuid
+        self.num_id = RandomPerson._instance_count
 
 
