cd0b88128087ba0d17fa8ae06099be032f998c15
Matthias Budde <buddemat@users.noreply.github.com>
2023-03-06 11:57:42 +0100

    Feat: Added getters for different formats.

--- random_person.py
+++ random_person.py
@@ -1,7 +1,9 @@
+import json
 from random import randint
 import uuid
 from datetime import datetime
 from pyzufall.person import Person
+from dicttoxml import dicttoxml
 
 class RandomPerson:
     """Class for random person"""
@@ -20,3 +22,12 @@ class RandomPerson:
         # ~2% should be diverse
         self.gender = 'diverse' if randint(0,100) < 2 else ('male' if p.geschlecht else 'female')
         self.birthname = p.geburtsname if p.geburtsname != p.nachname else None 
+
+    def to_dict(self):
+        return vars(self)
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=4, ensure_ascii=False)
+
+    def to_xml(self):
+        return dicttoxml(self.to_dict())
