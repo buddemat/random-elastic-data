519ddb0acae94331c22b694d7fc6cee3389e8350
Matthias Budde <buddemat@users.noreply.github.com>
2023-03-06 11:59:57 +0100

    Bugfix: Changed date to string to fix jsondumps error.

--- random_person.py
+++ random_person.py
@@ -11,7 +11,7 @@ class RandomPerson:
 
     def __init__(self, uuid=str(uuid.uuid1())):
         RandomPerson._instance_count += 1
-        self.created_at = datetime.today() # created_at.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
+        self.created_at = datetime.today().created_at.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         self.uuid = uuid
         self.num_id = RandomPerson._instance_count
 
