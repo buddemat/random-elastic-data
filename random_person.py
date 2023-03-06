59979559e61aae03032a13e02408a0d20feb7aec
Matthias Budde <buddemat@users.noreply.github.com>
2023-03-06 12:21:36 +0100

    Feat: Added additional attributes.

--- random_person.py
+++ random_person.py
@@ -4,6 +4,9 @@ import uuid
 from datetime import datetime
 from pyzufall.person import Person
 from dicttoxml import dicttoxml
+from faker import Faker
+
+faker = Faker()
 
 class RandomPerson:
     """Class for random person"""
@@ -19,9 +22,18 @@ class RandomPerson:
         self.firstname = p.vorname
         self.lastname = p.nachname
         self.nested_name = { 'first': p.vorname, 'last': p.nachname }
+        self.nickname = p.nickname
+        # TODO: make age attribute into property
+        self.age = p.alter
+        self.date_of_birth = p.geburtsdatum
         # ~2% should be diverse
         self.gender = 'diverse' if randint(0,100) < 2 else ('male' if p.geschlecht else 'female')
         self.birthname = p.geburtsname if p.geburtsname != p.nachname else None
+        self.birthplace = p.geburtsort
+        self.email_address = p.email
+        self.ip_address = faker.ipv4()
+        # ~10% should be lefthanded
+        self.lefthanded = bool(randint(0,100) < 10)
 
     def to_dict(self):
         return vars(self)
